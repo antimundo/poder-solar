ef6b81408a39ca34df0599bbe1099c49f5f123b3
Antimundo21 <pablo.rubio.ru@gmail.com>
2023-09-25 00:49:19 +0200

    Add tutorial

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -1,9 +1,40 @@
 extends Node
 
 @onready var end_scene = load("res://scenes/end_screen/end_scren.tscn")
+@onready var slides = $Slides
+
+var current_level: int = 0
+
+func _ready():
+	load_slides()
 
 func load_end_screen(end_state: end_screen.end_states):
 	var this_scene = end_scene.instantiate()
 	get_tree().root.add_child(this_scene)
 	this_scene.set_state(end_state)
 	queue_free()
+
+func load_level():
+	match current_level:
+		0:
+			pass
+		1:
+			pass
+		2:
+			pass
+
+func start_level():
+	var moon_tween: Tween = create_tween()
+	$Moon.position.x = 75
+	moon_tween.tween_property($Moon, "position:x", 750, 60)
+	current_level += 1
+	await moon_tween.finished
+	if current_level >= 3:
+		load_end_screen(end_screen.end_states.WIN)
+	else:
+		load_slides()
+
+func load_slides():
+	get_tree().paused = true
+	slides.visible = true
+	slides.load_first_slide(current_level)
---
+++ scenes/slides/slides.gd
@@ -0,0 +1,66 @@
+extends Control
+
+const game_slides = {
+	0: {
+		0: {
+			"text": "[center]¡Han instalado [b]placas solares[/b] en el pueblo!",
+			"image": "res://sprites/slides/win.jpg"
+		},
+		1: {
+			"text": "[center]Pero cuando [b]cae la noche[/b] nos quedamos sin electricidad :(",
+			"image": "res://sprites/slides/lights_out.jpg"
+		},
+		2: {
+			"text": "[center][b]Arrastra[/b] objetos a la pantalla para comprarlos",
+			"image": "res://sprites/slides/tutorial.jpg"
+		},
+		3: {
+			"text": "[center]No te quedes sin [b]electricidad[/b] o habrá un apagón",
+			"image": "res://sprites/slides/tutorial_lowenergy.jpg"
+		},
+		4: {
+			"text": "[center]Pero no te pases, demasiada electricidad producirá una [b]sobrecarga[/b]",
+			"image": "res://sprites/slides/tutorial_overcharge.jpg"
+		},
+		5: {
+			"text": "[center]Por último, no dejes que suba la [b]contaminación[/b] o habrá un desastre ecológico",
+			"image": "res://sprites/slides/tutorial_pollution.jpg"
+		}
+	},
+	1: {
+		0: {
+			"text": "[center]Hoy dan un concierto ¡Esto va a [b]aumentar[/b] en el consumo eléctrico!",
+			"image": "res://sprites/slides/win.jpg"
+		}
+	},
+	2: {
+		0: {
+			"text": "[center]Ahora en el pueblo contaminan",
+			"image": "res://sprites/slides/win.jpg"
+		}
+	}
+}
+
+@onready var load_scene = load("res://scenes/gameplay/gameplay.tscn")
+
+var current_slide: int = 0
+
+func load_first_slide(level: int):
+	load_slide(level, 0)
+
+func _on_button_pressed():
+	load_next_slide()
+
+func load_slide(level: int, slide: int):
+	$TextureRect.texture = load(game_slides[level][slide].image)
+	$Panel/RichTextLabel.text = game_slides[level][slide].text
+
+func load_next_slide():
+	current_slide += 1
+	var current_level: int = $"..".current_level
+	if current_slide < game_slides[current_level].size():
+		load_slide(current_level, current_slide)
+	else:
+		visible = false
+		get_tree().paused = false
+		$"..".start_level()

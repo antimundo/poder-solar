dd41fa289cd1cdc868f845114c021b072d6e7e76
Antimundo21 <pablo.rubio.ru@gmail.com>
2023-09-24 21:24:16 +0200

    Add end screen images

--- scenes/end_screen/end_scren.gd
+++ scenes/end_screen/end_scren.gd
@@ -7,18 +7,26 @@ enum end_states { WIN, LOOSE_POLLUTION, LOOSE_ENERGY_OVERCHARGE\
 @onready var title = $Title
 @onready var description = $Description
 @onready var main_menu = load("res://scenes/main_menu/main_menu.tscn")
+@onready var image: TextureRect = $Image
 
 func set_state(state: end_states):
 	match state:
 		end_states.WIN:
 			title.text = "¡Victoria!"
 			description.text = "Has ganado"
+			image.set_texture(load("res://sprites/slides/win.jpg"))
 		end_states.LOOSE_ENERGY_OVERCHARGE:
 			title.text = "¡Sobrecarga eléctrica!"
 			description.text = "Tanta energía ha quemado el tendido eléctrico :("
+			image.set_texture(load("res://sprites/slides/fire.jpg"))
 		end_states.LOOSE_POLLUTION:
-			title.text = "¡Demasiada contaminación!"
-			description.text = "Tanta contaminación ha provocado un desastre ecológico"
+			title.text = "¡Contaminación!"
+			description.text = "Tanta contaminación ha provocado un desastre ecológico :("
+			image.set_texture(load("res://sprites/slides/pollution.jpg"))
+		end_states.LOOSE_ENERGY_OUT:
+			title.text = "¡Apagón!"
+			description.text = "El pueblo se ha quedado sin electricidad :("
+			image.set_texture(load("res://sprites/slides/lights_out.jpg"))
 
 func _on_button_pressed():
 	var this_gameplay_scene = main_menu.instantiate()
